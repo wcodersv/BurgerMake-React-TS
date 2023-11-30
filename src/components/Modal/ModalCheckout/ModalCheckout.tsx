f27895882b9f1e48c8808dd36288f3f29d24f742
wcodersv <vik99a@yandex.ru>
2023-11-30 20:50:03 +0700

    rules for phone number

--- src/components/Modal/ModalCheckout/ModalCheckout.tsx
+++ src/components/Modal/ModalCheckout/ModalCheckout.tsx
@@ -1,7 +1,7 @@
 // ModalCheckout.tsx
 import React, { useEffect, useState } from 'react';
 import { DevTool } from "@hookform/devtools";
-import { useForm, FormProvider, SubmitHandler, FieldErrors } from "react-hook-form"
+import { useForm, FieldErrors, Controller } from "react-hook-form";
 import styles from './ModalCheckout.module.scss';
 import ModalWrapper from '../ModalWrapper';
 import ModalHeader from '../ModalHeader';
@@ -10,6 +10,12 @@ import InputWrapper from '../../form/Input/InputWrapper';
 import InputField from '../../form/Input/InputField';
 import InputLabel from '../../form/Input/InputLabel';
 import InputImg from '../../form/Input/InputImg';
+
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+import { parsePhoneNumberFromString } from 'libphonenumber-js';
+
+
 
 const inputList = {
     'username': {
@@ -38,7 +44,6 @@ export type FormValues = {
     username: string;
     phonenumber: string;
     shippingAddress: string;
-    example: string;
 }
 
 
@@ -48,18 +53,19 @@ export const ModalCheckout = ({ toggleModal }: ModalCheckoutProps) => {
             username: '',
             phonenumber: '',
             shippingAddress: '',
-            example: '',
         },
-        mode: 'onTouched'
+        mode: 'onTouched',
+        criteriaMode: 'all',
     });
 
 
     const { register, reset, control, handleSubmit, formState, getValues } = methods;
     const { errors, isDirty, isValid, isSubmitSuccessful } = formState;
 
-
-
     const [inputFocus, setInputFocus] = useState<string | null>(null);
+
+
+
 
     //*
     const handleInputFocus = (inputName: string) => {
@@ -91,125 +97,167 @@ export const ModalCheckout = ({ toggleModal }: ModalCheckoutProps) => {
     }, [isSubmitSuccessful, reset])
 
 
+
+
     return (
         <ModalWrapper >
             <ModalHeader title='Checkout' handle={toggleModal} />
-            <FormProvider {...methods}>
-                <form
-                    onSubmit={handleSubmit(onSubmitForm, onError)}
-                    className={styles.modal_form}
-                    noValidate
-                >
-                    <div className={styles.modal_form_main}>
-                        {/* firstName */}
-                        <InputWrapper onFocus={() => handleInputFocus('username')} onBlur={handleInputBlur} hasError={errors.username ? true : false}>
-                            <InputImg
-                                srcImg={
-                                    errors.username
-                                        ? inputList.username.imgError
-                                        : inputFocus === 'username'
-                                            ? inputList.username.imgActive
-                                            : inputList.username.imgDefault
-                                }
-                            />
-                            <InputField
-                                label="username"
-                                register={register}
-                                required='Required field'
-                                idHtmlFor='usernameinput'
-                                isInputFocused={inputFocus === 'username' || !!getValues('username')}
-                            />
-
-                            <InputLabel
-                                titleLabel='Your Name'
-                                idHtmlFor='usernameinput'
-                                isInputFocused={inputFocus === 'username' || !!getValues('username')}
-                                hasError={errors.username ? true : false}
-                            />
-                            <p className={styles.modal_error}>{errors.username?.message}</p>
-                        </InputWrapper>
-
-                        {/* Phone Number */}
-                        <InputWrapper onFocus={() => handleInputFocus('username')} onBlur={handleInputBlur} hasError={errors.username ? true : false}>
-                            <InputImg
-                                srcImg={
-                                    errors.username
-                                        ? inputList.username.imgError
-                                        : inputFocus === 'username'
-                                            ? inputList.username.imgActive
-                                            : inputList.username.imgDefault
-                                }
-                            />
-                            <InputField
-                                label="username"
-                                register={register}
-                                required='Required field'
-                                idHtmlFor='usernameinput'
-                                isInputFocused={inputFocus === 'username' || !!getValues('username')}
-                            />
-
-                            <InputLabel
-                                titleLabel='Your Name'
-                                idHtmlFor='usernameinput'
-                                isInputFocused={inputFocus === 'username' || !!getValues('username')}
-                                hasError={errors.username ? true : false}
-                            />
-                            <p className={styles.modal_error}>{errors.username?.message}</p>
-                        </InputWrapper>
-
-                        {/* email */}
-                        <InputWrapper onFocus={() => handleInputFocus('shippingAddress')} onBlur={handleInputBlur} hasError={errors.shippingAddress ? true : false}>
-                            <InputImg
-                                srcImg={
-                                    errors.shippingAddress
-                                        ? inputList.shippingAddress.imgError
-                                        : inputFocus === 'shippingAddress'
-                                            ? inputList.shippingAddress.imgActive
-                                            : inputList.shippingAddress.imgDefault
-                                }
-                            />
-                            <InputField
-                                label="shippingAddress"
-                                typeBtn='address'
-                                register={register}
-                                required='Required field'
-                                idHtmlFor='shippingaddressinput'
-                                isInputFocused={inputFocus === 'shippingAddress' || !!getValues('shippingAddress')}
-                            />
-
-                            <InputLabel
-                                titleLabel='Shipping Address'
-                                idHtmlFor='shippingaddressinput'
-                                isInputFocused={inputFocus === 'shippingAddress' || !!getValues('shippingAddress')}
-                                hasError={errors.shippingAddress ? true : false}
-                            />
-                            <p className={styles.modal_error}>{errors.shippingAddress?.message}</p>
-                        </InputWrapper>
-
-                    </div>
-
-
-                    <div className={styles.modal_footer}>
-                        <ButtonAction
-                            text='Cancel'
-                            backgroundColorBtn='var(--clr-titan-white)'
-                            colorText='var(--clr-primary)'
-                            widthBtn='7.5rem'
-                            handle={toggleModal}
+            <form
+                onSubmit={handleSubmit(onSubmitForm, onError)}
+                className={styles.modal_form}
+                noValidate
+            >
+                <div className={styles.modal_form_main}>
+                    {/* firstName */}
+                    <InputWrapper onFocus={() => handleInputFocus('username')} onBlur={handleInputBlur} hasError={errors.username ? true : false}>
+                        <InputImg
+                            srcImg={
+                                errors.username
+                                    ? inputList.username.imgError
+                                    : inputFocus === 'username'
+                                        ? inputList.username.imgActive
+                                        : inputList.username.imgDefault
+                            }
+                        />
+                        <InputField
+                            label="username"
+                            register={register}
+                            required='Required field'
+                            idHtmlFor='usernameinput'
+                            isInputFocused={inputFocus === 'username' || !!getValues('username')}
                         />
 
-                        <ButtonAction
-                            text='Checkout'
-                            backgroundColorBtn='var(--clr-primary)'
-                            colorText='var(--clr-titan-white)'
-                            widthBtn='7.5rem'
-                            typeBtn='submit'
-                            handle={() => onSubmitForm}
-                            disabled={!isDirty || !isValid}
+                        <InputLabel
+                            titleLabel='Your Name'
+                            idHtmlFor='usernameinput'
+                            isInputFocused={inputFocus === 'username' || !!getValues('username')}
+                            hasError={errors.username ? true : false}
                         />
-                    </div>
-                </form>
-            </FormProvider>
+                        <p className={styles.modal_error}>{errors.username?.message}</p>
+                    </InputWrapper>
+
+                    {/* Phone Number */}
+                    <InputWrapper onFocus={() => handleInputFocus('phonenumber')} onBlur={handleInputBlur} hasError={errors.phonenumber ? true : false}>
+                        <InputImg
+                            srcImg={
+                                errors.phonenumber
+                                    ? inputList.phonenumber.imgError
+                                    : inputFocus === 'phonenumber'
+                                        ? inputList.phonenumber.imgActive
+                                        : inputList.phonenumber.imgDefault
+                            }
+                        />
+
+                        <Controller
+                            name='phonenumber'
+                            control={control}
+                            rules={{
+                                required: 'Required fiel',
+                                validate: {
+                                    validPhoneNumber: (value) => {
+                                        try {
+                                            const phoneNumber = parsePhoneNumberFromString(`+${value}`);
+                                      
+
+                                            if (phoneNumber && phoneNumber.isValid()) {
+                                                const fullNumber = `+${phoneNumber.countryCallingCode}${phoneNumber.nationalNumber}`;
+                                                return true;
+                                            } else {
+                                                console.error('Invalid phone number format');
+                                                return false;
+                                            }
+
+                                        } catch (error) {
+                                            console.error('Error validating phone number:', error);
+                                            return false;
+                                        }
+
+                                    },
+                                },
+                            }}
+                            render={({ field }) => (
+                                <PhoneInput
+                                    {...field}
+                                    inputProps={{
+                                        id: 'phoneInput',
+                                    }}
+                                    placeholder=''
+                                    containerClass={styles.phoneinput_container}
+                                    inputClass={`${styles.phoneinput_input} ${inputFocus === 'phonenumber' || !!getValues('phonenumber')
+                                        ? styles.phoneinput_input_focused
+                                        : ''
+                                        }`}
+                                    buttonClass={styles.phoneinput_country}
+                                />
+                            )}
+                        />
+
+                        <InputLabel
+                            titleLabel='Phone Number'
+                            idHtmlFor='phoneInput'
+                            isInputFocused={inputFocus === 'phonenumber' || !!getValues('phonenumber')}
+                            hasError={errors.phonenumber ? true : false}
+                        />
+                        <p className={styles.modal_error}>{errors.phonenumber?.message}</p>
+                    </InputWrapper>
+
+                    {/* email */}
+                    <InputWrapper onFocus={() => handleInputFocus('shippingAddress')} onBlur={handleInputBlur} hasError={errors.shippingAddress ? true : false}>
+                        <InputImg
+                            srcImg={
+                                errors.shippingAddress
+                                    ? inputList.shippingAddress.imgError
+                                    : inputFocus === 'shippingAddress'
+                                        ? inputList.shippingAddress.imgActive
+                                        : inputList.shippingAddress.imgDefault
+                            }
+                        />
+                        <InputField
+                            label="shippingAddress"
+                            typeInput='address'
+                            register={register}
+                            required='Required field'
+                            idHtmlFor='shippingaddressinput'
+                            isInputFocused={inputFocus === 'shippingAddress' || !!getValues('shippingAddress')}
+                        />
+
+                        <InputLabel
+                            titleLabel='Shipping Address'
+                            idHtmlFor='shippingaddressinput'
+                            isInputFocused={inputFocus === 'shippingAddress' || !!getValues('shippingAddress')}
+                            hasError={errors.shippingAddress ? true : false}
+                        />
+                        <p className={styles.modal_error}>{errors.shippingAddress?.message}</p>
+                    </InputWrapper>
+
+
+
+
+
+                </div>
+
+
+                <div className={styles.modal_footer}>
+                    <ButtonAction
+                        text='Cancel'
+                        backgroundColorBtn='var(--clr-titan-white)'
+                        colorText='var(--clr-primary)'
+                        widthBtn='7.5rem'
+                        handle={toggleModal}
+                    />
+
+                    <ButtonAction
+                        text='Checkout'
+                        backgroundColorBtn='var(--clr-primary)'
+                        colorText='var(--clr-titan-white)'
+                        widthBtn='7.5rem'
+                        typeBtn='submit'
+                        handle={() => onSubmitForm}
+                        disabled={!isDirty || !isValid}
+                    />
+                </div>
+            </form>
+
 
             {/*! Удалить в самом конце проекта */}
             <DevTool control={control} />
--- src/components/form/Input/InputField/InputField.tsx
+++ src/components/form/Input/InputField/InputField.tsx
@@ -2,24 +2,30 @@
 import React from 'react';
 import styles from './InputField.module.scss';
 import { FormValues } from '../../../Modal/ModalCheckout/ModalCheckout';
-import { Path, UseFormRegister } from "react-hook-form";
+import { Path, UseFormRegister, ValidationRule } from "react-hook-form";
 
 type InputFieldProps = {
     label: Path<FormValues>;
+
     register: UseFormRegister<FormValues>;
-    required: string;
-    typeBtn?: string;
+    required?: string;
+    pattern?: ValidationRule<RegExp>;
+    typeInput?: string;
     idHtmlFor: string;
     isInputFocused?: boolean;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 };
 
-export const InputField = ({ label, register, required, idHtmlFor, isInputFocused, typeBtn = 'text' }: InputFieldProps) => {
+export const InputField = ({ label, register, required, pattern, idHtmlFor, isInputFocused, onChange, onKeyDown, typeInput = 'text' }: InputFieldProps) => {
     return (
         <input
             className={`${styles.input_field} ${isInputFocused ? styles.input_focused : ''}`}
-            type={typeBtn}
+            type={typeInput}
             id={idHtmlFor}
-            {...register(label, { required })}
+            {...register(label, { required, pattern })}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
         />
     )
 }
