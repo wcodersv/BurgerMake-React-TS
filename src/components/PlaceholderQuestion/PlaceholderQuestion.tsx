256d1e3aa575cc8c0dd4d319a953077fc758e2af
wcodersv <vik99a@yandex.ru>
2023-12-04 15:15:22 +0700

    src img

--- src/components/PlaceholderQuestion/PlaceholderQuestion.tsx
+++ src/components/PlaceholderQuestion/PlaceholderQuestion.tsx
@@ -4,7 +4,7 @@ import styles from './PlaceholderQuestion.module.scss';
 export const PlaceholderQuestion = () => {
     return (
         <div className={styles.placeholder}>
-            <img src="/assets/svg/icon-emoji-boom.svg" alt=" " className={styles.placeholder__img} />
+            <img src={`${process.env.PUBLIC_URL}/assets/svg/icon-emoji-boom.svg`} alt=" " className={styles.placeholder__img} />
             <p className={styles.placeholder__content}>You are sure?</p>
         </div>
     )
