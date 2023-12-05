3550f9b8bd8bf93bf005453018999b06e2492ad2
wcodersv <vik99a@yandex.ru>
2023-12-05 18:14:31 +0700

    edit burger whole

--- src/components/BurgerWhole/BurgerWhole.tsx
+++ src/components/BurgerWhole/BurgerWhole.tsx
@@ -1,5 +1,5 @@
 // BurgerWhole.tsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './BurgerWhole.module.scss';
 import BurgerPiece from '../BurgerPiece';
 
@@ -14,13 +14,38 @@ interface BurgerWholeProps {
         left: string;
         transform?: string;
     }[];
+
 }
 
 export const BurgerWhole: React.FC<BurgerWholeProps> = ({ ingredients = [] }) => {
+    const [imagesLoaded, setImagesLoaded] = useState<boolean>(false);
+
+    useEffect(() => {
+        const loadImage = (src: string) => {
+            return new Promise<void>((resolve) => {
+                const image = new Image();
+                image.src = src;
+                image.onload = () => resolve();
+            });
+        };
+
+        const loadImages = async () => {
+            try {
+                const promises = ingredients.map((ingredient) => loadImage(ingredient.src));
+                await Promise.all(promises);
+                setImagesLoaded(true);
+            } catch (error) {
+                console.error('Error loading images:', error);
+            }
+        };
+
+        loadImages();
+    }, [ingredients]);
+
 
     return (
         <div className={styles.burger}>
-            {
+            {imagesLoaded &&
                 ingredients.map((ingredient, index) => (
                     <BurgerPiece
                         key={index}
@@ -32,8 +57,7 @@ export const BurgerWhole: React.FC<BurgerWholeProps> = ({ ingredients = [] }) =>
                         position={{ top: ingredient.top, left: ingredient.left }}
                         transform={ingredient.transform}
                     />
-                ))
-            }
+                ))}
         </div>
     );
 };
